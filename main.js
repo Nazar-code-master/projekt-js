f1bb6979b248ebd07249823a18c6496f831bf30a
Nazar-code-master <nazarii.rymarenko@skole.hr>
2026-06-27 14:20:15 +0200

    add a js code

---
+++ main.js
@@ -0,0 +1,69 @@
+let fish1 = document.querySelector('.fish1');
+let fish2 = document.querySelector('.fish2');
+let fish3 = document.querySelector('.fish3');
+let fish4 = document.querySelector('.fish4');
+let fish5 = document.querySelector('.fish5');
+let fish6 = document.querySelector('.fish6');
+let number = document.querySelector('.number');
+let count = 0;
+
+fish1.onclick = function () {
+    if
+        (!fish1.classList.contains('active')) {
+        count = count + 1;
+        number.innerHTML = count;
+        fish1.classList.add('active');
+    }
+
+
+}
+fish2.onclick = function () {
+    if
+        (!fish2.classList.contains('active')) {
+        count = count + 1;
+        number.innerHTML = count;
+        fish2.classList.add('active');
+    }
+
+
+}
+fish3.onclick = function () {
+    if
+        (!fish3.classList.contains('active')) {
+        count = count + 1;
+        number.innerHTML = count;
+        fish3.classList.add('active');
+    }
+
+
+}
+fish4.onclick = function () {
+    if
+        (!fish4.classList.contains('active')) {
+        count = count + 1;
+        number.innerHTML = count;
+        fish4.classList.add('active');
+    }
+
+
+}
+fish5.onclick = function () {
+    if
+        (!fish5.classList.contains('active')) {
+        count = count + 1;
+        number.innerHTML = count;
+        fish5.classList.add('active');
+    }
+
+
+}
+fish6.onclick = function () {
+    if
+        (!fish6.classList.contains('active')) {
+        count = count + 1;
+        number.innerHTML = count;
+        fish6.classList.add('active');
+    }
+
+
+}
