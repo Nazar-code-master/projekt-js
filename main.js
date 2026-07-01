4a64789b1997340035a6dcab6e843412813a0652
Nazar-code-master <nazarii.rymarenko@skole.hr>
2026-07-01 19:46:46 +0200

    the last update 😊(●'◡'●)（￣︶￣）↗　

--- main.js
+++ main.js
@@ -69,27 +69,54 @@ fish6.onclick = function () {
 }
 
 
-let button1 = document.querySelector('.1a');
-let button2 = document.querySelector('.2a');
-let button3 = document.querySelector('.3a');
-let button4 = document.querySelector('.4a');
+let button1 = document.querySelector('.btn-1a');
+let button2 = document.querySelector('.btn-2a');
+let button3 = document.querySelector('.btn-3a');
+let button4 = document.querySelector('.btn-4a');
 let depthImage = document.querySelector('.depth-image');
 let depthTitle = document.querySelector('.deaph-title');
 let depthDescription = document.querySelector('.depth-description');
 let dp = document.querySelector('.dp');
-let depthTemp = document.querySelector(".depthTemp")
+let depthTemp = document.querySelector(".depth-temp")
+let depthInfo = document.querySelector(".depth-info")
 
 button1.onclick = function () {
+    button1.classList.toggle('active');
     depthTemp.innerHTML = "🌡️Temp: 24°C";
     depthTitle.innerHTML = "The Sunlit Surface";
     depthDescription.innerHTML = "The epipelagic zone receives ample sunlight, fueling photosynthesis and supporting the majority of ocean life, including playful dolphins andcoastal crabs.";
-    depthImage.src = "images/e923a075055432cff2886793c8eb3185b1455c60.png";
+    depthImage.src = "img/e923a075055432cff2886793c8eb3185b1455c60.png";
     dp.innerHTML = "0m Depth Range";
+    depthInfo.style.backgroundColor = "#CDE8FE";
+    depthInfo.style.color = "#005482";
 }
 button2.onclick = function () {
-    depthTemp.innerHTML = "🌡️Temp: 10°C";
+    button2.classList.toggle('active');
+    depthTemp.innerHTML = "🌡️Temp: 8°C";
     depthTitle.innerHTML = "The Twilight Zone";
-    depthDescription.innerHTML = "The mesopelagic zone is a dimly lit region where sunlight fades, and bioluminescent creatures like lanternfish and squid thrive in the mysterious depths.";
-    depthImage.src = "images/mesopelagic-zone.png";
-    dp.innerHTML = "200m Depth Range";
+    depthDescription.innerHTML = "Sunlight rapidly fades in the mesopelagic zone. Bioluminescence begins to appear as glowing jellyfish and squid navigate the dim, cool waters.";
+    depthImage.src = "img/33.png";
+    dp.innerHTML = "-500m Depth Range";
+    depthInfo.style.backgroundColor = "#D5E5EF";
+    depthInfo.style.color = "#005482";
+}
+button3.onclick = function () {
+    button3.classList.toggle('active');
+    depthTemp.innerHTML = "🌡️Temp: 2°C";
+    depthTitle.innerHTML = "The Abyss";
+    depthDescription.innerHTML = "Complete darkness and immense pressure characterize the bathypelagic zone. Strange creatures like the Anglerfish use bioluminescent lures to survive.";
+    depthImage.src = "img/44.png";
+    dp.innerHTML = "-1000m Depth Range";
+    depthInfo.style.backgroundColor = "#004871";
+    depthInfo.style.color = "#FFFFFF";
+}
+button4.onclick = function () {
+    button4.classList.toggle('active');
+    depthTemp.innerHTML = "🌡️Temp: 1°C";
+    depthTitle.innerHTML = "The Mariana Trench";
+    depthDescription.innerHTML = "The hadalpelagic zone, the deepest trenches on Earth. Pitch black and near-freezing, home to highly specialized life forms like pale snailfish.";
+    depthImage.src = "img/55.png";
+    dp.innerHTML = "-2000m + Depth Range";
+    depthInfo.style.backgroundColor = "#00192A";
+    depthInfo.style.color = "#FFFFFF";
 }
